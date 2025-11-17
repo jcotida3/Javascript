cbb961fa320058caf4e3b7f160649302d1c9c3b0
jcotida3 <jcotida3@gmail.com>
2025-11-17 13:41:07 +0800

    this is sample

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+const display = document.getElementById("display");
+
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = null;
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
+
+let x = Math.random() + 7;
+console.log(x);
